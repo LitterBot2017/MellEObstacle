e26b6d1f5bf0e584a8c49330cba4c6fc0f67b978
Nima Rahnemoon <nrahnemoon@gmail.com>
2017-02-13 18:15:19 -0500

    added message to potfields

--- src/hokPotFields.py
+++ src/hokPotFields.py
@@ -31,12 +31,6 @@ def callback(stuff):
 		if not math.isnan(stuff.angle_min + stuff.angle_increment*i) and not math.isnan(3/(1+stuff.ranges[i])):
 			directionVec[0][i] = stuff.angle_min + stuff.angle_increment*i
 			weightVec[0][i] = 3/(1+stuff.ranges[i])
-			#rospy.loginfo(directionVec[0][i])
-
-
-	
-
-
 
 	xDirVec = np.cos(directionVec)
 	yDirVec = np.sin(directionVec)
@@ -48,72 +42,14 @@ def callback(stuff):
 	dirDeg = np.rad2deg(totalDir)
 	negDir = totalDir - np.pi
 	negDirDeg = np.rad2deg(negDir)
+	msg = ObAvData()
+	msg.direction = dirDeg
+	msg.magnitude = totalMag
+	ob_av_data_pub.publish(msg)
 	rospy.loginfo(dirDeg)
 	rospy.loginfo(totalMag)
 
-	#rospy.loginfo(directionVec)
 
-'''
-
-	#stop if something is in either testicle
-
-	if min(stuff.ranges[40:180]) < 0.5 and min(stuff.ranges[340:480]) < 0.5:
-		command = 3
-
-
-
-	elif min(stuff.ranges[160:360]) < 1 or min(stuff.ranges[40:480]) < 0.5 :
-
-		# Penis
-		i = 160
-		for distance in stuff.ranges[160:360]:
-			if distance < 2 and distance != float('inf') and distance != float('nan'):
-				if i < 260:
-					leftWeight = leftWeight + (1/distance)
-				else:
-					rightWeight = rightWeight + (1/distance)
-			i += 1
-
-		# Left Testicle
-		i = 40
-		for distance in stuff.ranges[40:160]:
-			if distance < 0.5 and distance != float('inf') and distance != float('nan'):
-				leftWeight = leftWeight + (1/distance)
-			i += 1
-
-		# Right Testicle
-		i = 360
-		for distance in stuff.ranges[360:480]:
-			if distance < 0.5 and distance != float('inf') and distance != float('nan'):
-				rightWeight = rightWeight + (1/distance)
-			i += 1
-
-		if leftWeight > rightWeight:
-			rospy.loginfo("left")
-			command = 2
-		else:
-			rospy.loginfo("right")
-			command = 1
-		leftWeight = 0.0
-		rightWeight = 0.0
-	else:
-		rospy.loginfo("go")
-
-	msg = ObAvData()
-	msg.command = command
-
-	ob_av_data_pub.publish(msg)
-'''
-'''
-def callback(stuff):
-	ranges = stuff.ranges
-	angles = stuff.angles
-	#normalize the ranges
-	lineardistances = ranges * cos(angles)
-	#if min of ranges between someangleleft and someangleright
-
-
- ''' 
 def laser_reader():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
@@ -124,8 +60,8 @@ def laser_reader():
     rospy.init_node('laser_reader', anonymous=True)
 
     rospy.Subscriber("/scan",LaserScan,callback)
-   # global ob_av_data_pub 
-   # ob_av_data_pub = rospy.Publisher("/ob_av_data", ObAvData)
+    global ob_av_data_pub 
+    ob_av_data_pub = rospy.Publisher("/ob_av_data", ObAvData)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
